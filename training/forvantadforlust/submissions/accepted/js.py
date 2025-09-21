b0f1d22370c308ce2dce7fe8edc58c92e99f7038
Matistjati <matistjati@outlook.com>
2025-09-21 21:22:17 +0200

    Remove raknefunktioner

--- training/forvantadforlust/submissions/accepted/js.py
+++
@@ -1,3 +0,0 @@
-#@EXPECTED_RESULT@: AC
-n, k, p = [float(x) for x in raw_input().split()]
-print("Spela" if n*p - k < 0 else "Spela inte!")
--- training/raknefunktioner/include/cpp/raknefunktioner.h
+++
@@ -1,3 +0,0 @@
-int cube(int x);
-int max(int x, int y);
-int differens(int x, int y);
