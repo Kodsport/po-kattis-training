eacb477d1470c90841599384a0875ee4a064fcd9
Matistjati <matistjati@outlook.com>
2025-10-16 02:35:25 +0200

    Remove unneeded scoring text

---
+++ cng/pellesarmhavningarsvar/data/generator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+. ../../testdata_tools/gen.sh
+
+use_solution joshua.py
+
+compile print.py
+
+samplegroup
+sample 1
+sample 2
+sample 3
+
+group group1 100
+include_group sample
+tc g1-1 print
+tc g1-2 print
+tc g1-3 print
+tc g1-4 print
+tc g1-5 print
+tc g1-6 print
+tc g1-7 print
+tc g1-8 print
+tc g1-9 print
